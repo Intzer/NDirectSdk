0cf0acae5a3a03f476a47b7a37dc880f6b61f485
Roman <rmilh1@mail.ru>
2026-04-13 21:16:59 +0300

    fix chart Ox order

--- chart_iframe.php
+++ chart_iframe.php
@@ -5,17 +5,36 @@ use NDirectSdk\NDirectClient;
 
 $integration = NDirectClient::getInstance()->get();
 
-if (!$integration) {
+if (!$integration) 
+{
+    http_response_code(500);
     exit("SDK не инициализирован");
 }
 
 $type = $_GET['type'] ?? 'unique';
-$days = (int) ($_GET['days'] ?? 7);
+$days = max(1, min((int) ($_GET['days'] ?? 7), 90));
 
-switch ($type) {
+switch ($type) 
+{
     case 'installs': $data = $integration->getInstalls($days); break;
     case 'connects': $data = $integration->getConnects($days); break;
     default: $data = $integration->getUnique($days); break;
+}
+
+if (!empty($data)) 
+{
+    $dataArray = json_decode($data, true);
+
+    if (json_last_error() === JSON_ERROR_NONE && is_array($dataArray)) 
+    {
+        $dataArray = array_reverse($dataArray);
+        $data = json_encode($dataArray);
+    } 
+    else 
+    {
+        http_response_code(500);
+        exit("Ошибка отрисоки графика");
+    }
 }
 
 echo $integration->renderChart($data);
